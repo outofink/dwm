f7aef68a3b1e6c24553faa64a092175fe0463bb1
Moshe Krumbein <moshekrum@gmail.com>
2022-11-16 14:14:27 +0200

    fixed warnings

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;//1        /* border pixel of windows */
+static const unsigned int borderpx  = 0;	    /* border pixel of windows */
 static const unsigned int snap      = 16;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -88,64 +88,68 @@ static Key keys[] = {
 	/* modifier                     key			function		argument */
 	/*workspaces*/
 	TAGKEYS(                        XK_1,                       0)
-		TAGKEYS(                        XK_2,                       1)
-		TAGKEYS(                        XK_3,                       2)
-		TAGKEYS(                        XK_4,                       3)
-		TAGKEYS(                        XK_5,                       4)
-		TAGKEYS(                        XK_6,                       5)
-		TAGKEYS(                        XK_7,                       6)
-		TAGKEYS(                        XK_8,                       7)
-		TAGKEYS(                        XK_9,						8)
-		{ MODKEY|ShiftMask,             XK_q,		quit,			{0} },
-		{ MODKEY,						XK_q,		killclient,		{0} },
-		/*media controls*/
-		{ 0,				XF86XK_AudioLowerVolume,	spawn,		SHCMD("pamixer --allow-boost -d 5; kill -44 $(pidof dwmblocks)") },
-		{ ShiftMask,		XF86XK_AudioLowerVolume,	spawn,		SHCMD("pamixer --allow-boost -d 15; kill -44 $(pidof dwmblocks)") },
-		{ 0,				XF86XK_AudioRaiseVolume,	spawn,		SHCMD("pamixer --allow-boost -i 5; kill -44 $(pidof dwmblocks)") },
-		{ ShiftMask,		XF86XK_AudioRaiseVolume,	spawn,		SHCMD("pamixer --allow-boost -i 15; kill -44 $(pidof dwmblocks)") },
-		{ 0,				XF86XK_AudioPlay,			spawn,		SHCMD("playerctl play-pause") },
-		{ 0,				XF86XK_AudioMute,			spawn,		SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)") },
-		{ MODKEY,	        XK_comma,					spawn,	   	SHCMD("playerctl previous; kill -55 $(pidof dwmblocks)")},
-		{ MODKEY,	        XK_period,					spawn,	   	SHCMD("playerctl next; kill -55 $(pidof dwmblocks)")},
-		{ 0, 				XF86XK_MonBrightnessUp,		spawn,		SHCMD("xbacklight -inc 10; kill -45 $(pidof dwmblocks)") },
-		{ 0, 				XF86XK_MonBrightnessDown,	spawn,		SHCMD("xbacklight -dec 10; kill -45 $(pidof dwmblocks)") },
-		{ ShiftMask, 		XF86XK_MonBrightnessUp,		spawn,		SHCMD("xbacklight -inc 1; kill -45 $(pidof dwmblocks)") },
-		{ ShiftMask, 		XF86XK_MonBrightnessDown,	spawn,		SHCMD("xbacklight -dec 1; kill -45 $(pidof dwmblocks)") },
-		/*shortcuts*/
-		{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-		{ MODKEY,	                    XK_Return, spawn,          {.v = termcmd } },
-		{ MODKEY|ShiftMask,	            XK_Return, togglescratch,  {.ui = 0}},
-		{ MODKEY,	                    XK_i,      togglescratch,  {.ui = 1}},
-		{ MODKEY,	                    XK_slash,  togglescratch,  {.ui = 2}},
-		{ MODKEY,	               	    XK_c, 	   spawn,          SHCMD("brave")},
-		{ MODKEY,	               	    XK_t, 	   spawn,          SHCMD("thunar")},
-		{ MODKEY,	               	    XK_w, 	   spawn,          SHCMD("whatsapp-nativefier")},
-		{ MODKEY,	               	    XK_s, 	   spawn,          SHCMD("spotify")},
-		{ MODKEY,	               	    XK_b, 	   spawn,          SHCMD("rofi-bt")},
-		{ MODKEY,	               	    XK_Escape, spawn,		   SHCMD("lock")},
-		{ MODKEY|ShiftMask,	     	    XK_Escape, spawn,		   SHCMD("rofi-power")},
-		{ MODKEY,	             	    XK_d,      spawn,		   SHCMD("rofi -modi combi -combi-modi window#drun#run -show combi")},
-		{ MODKEY,	             	    XK_v,      spawn,		   SHCMD("alacritty -e nvim")},
-		{ MODKEY,	             	    XK_r,      spawn,		   SHCMD("pkill dwmblocks; sleep 2; dwmblocks &")},
-		{ 0,		             	    XK_Print,  spawn,		   SHCMD("scrot -s -m ~/pics/screenshots/%s_%H%M_%d.%m.%Y.png")},
-		/**/
-		{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
-		{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-		{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-		{ MODKEY,                       XK_o,      incnmaster,     {.i = +1 } },
-		{ MODKEY|ShiftMask,             XK_o,      incnmaster,     {.i = -1 } },
-		{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-		{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-		{ MODKEY,                       XK_space,  zoom,           {0} },
-		{ MODKEY,                       XK_Tab,    view,           {0} },
-		{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[0]} },
-		{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
-		{ MODKEY,			            XK_f,      togglefullscr,  {0} },
-		{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-		{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-		{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-		{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-		};
+	TAGKEYS(                        XK_2,                       1)
+	TAGKEYS(                        XK_3,                       2)
+	TAGKEYS(                        XK_4,                       3)
+	TAGKEYS(                        XK_5,                       4)
+	TAGKEYS(                        XK_6,                       5)
+	TAGKEYS(                        XK_7,                       6)
+	TAGKEYS(                        XK_8,                       7)
+	TAGKEYS(                        XK_9,						8)
+	{ MODKEY|ShiftMask,             XK_q,		quit,			{0} },
+	{ MODKEY,						XK_q,		killclient,		{0} },
+	/*media controls*/
+	{ 0,				XF86XK_AudioLowerVolume,	spawn,		SHCMD("pamixer --allow-boost -d 5; kill -44 $(pidof dwmblocks)") },
+	{ ShiftMask,		XF86XK_AudioLowerVolume,	spawn,		SHCMD("pamixer --allow-boost -d 15; kill -44 $(pidof dwmblocks)") },
+	{ 0,				XF86XK_AudioRaiseVolume,	spawn,		SHCMD("pamixer --allow-boost -i 5; kill -44 $(pidof dwmblocks)") },
+	{ ShiftMask,		XF86XK_AudioRaiseVolume,	spawn,		SHCMD("pamixer --allow-boost -i 15; kill -44 $(pidof dwmblocks)") },
+	{ 0,				XF86XK_AudioPlay,			spawn,		SHCMD("playerctl play-pause") },
+	{ 0,				XF86XK_AudioMute,			spawn,		SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)") },
+	{ MODKEY,	        XK_comma,					spawn,	   	SHCMD("playerctl previous; kill -55 $(pidof dwmblocks)")},
+	{ MODKEY,	        XK_period,					spawn,	   	SHCMD("playerctl next; kill -55 $(pidof dwmblocks)")},
+	{ 0, 				XF86XK_MonBrightnessUp,		spawn,		SHCMD("xbacklight -inc 10; kill -45 $(pidof dwmblocks)") },
+	{ 0, 				XF86XK_MonBrightnessDown,	spawn,		SHCMD("xbacklight -dec 10; kill -45 $(pidof dwmblocks)") },
+	{ ShiftMask, 		XF86XK_MonBrightnessUp,		spawn,		SHCMD("xbacklight -inc 1; kill -45 $(pidof dwmblocks)") },
+	{ ShiftMask, 		XF86XK_MonBrightnessDown,	spawn,		SHCMD("xbacklight -dec 1; kill -45 $(pidof dwmblocks)") },
+	/*shortcuts*/
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,	                    XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,	            XK_Return, togglescratch,  {.ui = 0}},
+	{ MODKEY,	                    XK_i,      togglescratch,  {.ui = 1}},
+	{ MODKEY,	                    XK_slash,  togglescratch,  {.ui = 2}},
+	{ MODKEY,	               	    XK_c, 	   spawn,          SHCMD("brave")},
+	{ MODKEY,	               	    XK_t, 	   spawn,          SHCMD("thunar")},
+	{ MODKEY,	               	    XK_w, 	   spawn,          SHCMD("whatsapp-nativefier")},
+	{ MODKEY,	               	    XK_s, 	   spawn,          SHCMD("spotify")},
+	{ MODKEY,	               	    XK_b, 	   spawn,          SHCMD("rofi-bt")},
+	{ MODKEY,	               	    XK_Escape, spawn,		   SHCMD("lock")},
+	{ MODKEY|ShiftMask,	     	    XK_Escape, spawn,		   SHCMD("rofi-power")},
+	{ MODKEY,	             	    XK_d,      spawn,		   SHCMD("rofi -modi combi -combi-modi window#drun#run -show combi")},
+	{ MODKEY,	             	    XK_v,      spawn,		   SHCMD("alacritty -e nvim")},
+	{ MODKEY,	             	    XK_r,      spawn,		   SHCMD("pkill dwmblocks; sleep 2; dwmblocks &")},
+	{ 0,		             	    XK_Print,  spawn,		   SHCMD("scrot -s -m ~/pics/screenshots/%s_%H%M_%d.%m.%Y.png")},
+	/**/
+	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_o,      incnmaster,     {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_o,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_space,  zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,			            XK_f,      togglefullscr,  {0} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+};
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
